22a15ae308c56ad2a0431c1ebd4801236bfee6ee
jcrab <jcrabtree@una.edu>
2019-04-06 20:36:39 -0500

    Add files via upload

---
+++ hash_example.py
@@ -0,0 +1,18 @@
+# example of iterating a nonce in a hashing algorithm's input
+# updated for python3 -- jdc
+
+import hashlib
+
+text = "I am Satoshi Nakamoto"
+
+# iterate nonce from 0 to 19
+for nonce in range(20):
+
+    # add the nonce to the end of the text
+    input_data = text + str(nonce)
+
+    # calculate the SHA-256 hash of the input (text+nonce)
+    hash_data = hashlib.sha256(input_data.encode('utf-8')).hexdigest()
+
+    # show the input and hash result
+    print(input_data, '=>', hash_data)
